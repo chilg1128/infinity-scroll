5f911655590a1de9ac4bcd5284e74f7a3bdb1d6f
Daniel Ilg <Daniel.Ilg@nielsen.com>
2020-10-26 15:26:05 -0400

    added displayPhotos function

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 //Unsplash API
 const count = 10;
 const apiKey = 'tul3E0ucKBUg3MaztcjiGZnAClTJNH0MbKazFVTUe1o';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Create Elements for links & photos, Add to DOM
+function displayPhotos() {
+    //run function foreach object in photosArray
+    photosArray.forEach((photo) => {
+        //Create <a> element to link to Unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+
+        //Create img for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+
+        //Put <img> inside <a>, then put both inside imageContainer
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
 // Get photos from Unsplash API
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        console.log(photosArray);
     } catch (error) {
         //Catch Error
 
